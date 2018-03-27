e8eeaa03ed7d9999de38493e19d0759b7208220c
Unknown <l.bourdrel@laposte.net>
2018-03-27 18:42:10 +0200

    AI 1 OK

---
+++ Assets/Script/AI_Trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Trigger : MonoBehaviour {
+
+    public IA_Creep AI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AI.Tease();
+        }
+    }
+}
--- Assets/Script/IA_Creep.cs
+++ Assets/Script/IA_Creep.cs
@@ -6,17 +6,29 @@ using UnityEngine.AI;
 public class IA_Creep : MonoBehaviour {
 
 	NavMeshAgent agent;
-	public Transform player;
-    public Animator anim;
+
+    [Header("Waypoints_Tease")]
+    public Transform[] waypointsTease;
+
+    public Transform player;
+    public Animator animator;
 
 	void Start () 
 	{
 		agent = GetComponent<NavMeshAgent> ();
 	}
-	
 
-	void LateUpdate () 
-	{
-		agent.destination = player.position;
-	}
+    public void Tease()
+    {
+        StartCoroutine(TeaseCorou());
+    }
+
+    public IEnumerator TeaseCorou()
+    {
+        yield return new WaitForSeconds(2f);
+        agent.destination = waypointsTease[0].position;
+        animator.SetTrigger("Walk");
+        yield return new WaitForSeconds(3f);
+        agent.destination = waypointsTease[1].position;
+    }
 }
